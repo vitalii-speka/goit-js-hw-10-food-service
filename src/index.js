219b7fe0ce611cb3195f1bfcd0b5c63b9bbc5622
Vitalii Speka <spekava@gmail.com>
2020-10-22 00:27:22 +0300

    добавил классы на событе `change`,
    не понятно, как добарться к значению чекед!

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
 import menuCardTpl from './templates/card-menu.hbs';
-// import templateFunction from '/templates/color-card.hbs';
-// document.body.innerHTML = templateFunction();
 import '../src/css/styles.css';
 // import throttle from 'lodash.throttle';
-// import colorCardTpl from '.src/templates/color-card.hbs';
-
 import menu from '../src/menu.json';
 
 const menuRef = document.querySelector('.js-menu');
-
 const markup = menuCardTpl(menu);
+const themeChenge = document.querySelector('#theme-switch-toggle');
+const valueSwitchToggle = document.querySelector('theme-switch__toggle');
+
 menuRef.insertAdjacentHTML('beforeend', markup);
-console.log(menuRef);
+
+themeChenge.addEventListener('change', onThemeChenge)
+
+
+function onThemeChenge(e) {
+    e.preventDefault();
+    addClassList()
+    console.log(valueSwitchToggle)
+    console.log(e)
+
+}
+
+function addClassList() {
+    document.querySelector('body').classList.add(Theme.LIGHT);
+    document.querySelector('body').classList.toggle(Theme.DARK);
+
+//   document.querySelector('body').classList.replace(Theme.LIGHT, Theme.DARK);
+}
+
+// console.log(themeChenge);
+
+const Theme = {
+  LIGHT: "light-theme",
+  DARK: "dark-theme",
+};
+
+console.log(Theme);
 
 console.dir(menuCardTpl);
-console.dir(ingredients);
